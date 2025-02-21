abc2c5c7897b10770681063467e9c259a234c075
Yash <starrai843@gmail.com>
2025-02-21 17:35:21 +0530

    Fix CORS issue

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 app.use(express.json()); 
+app.use(cors());
 
 app.post("/bfhl", (req, res) => {
     const { data } = req.body;
-    
+
+    if (!Array.isArray(data)) {
+        return res.status(400).json({
+            is_success: false,
+            message: "Invalid input. 'data' must be an array."
+        });
+    }
+
     const numbers = data.filter((item) => !isNaN(item));
     const alphabets = data.filter((item) => isNaN(item));
     const highestAlphabet = alphabets.length ? alphabets.sort().pop().toUpperCase() : null;
@@ -13,7 +21,7 @@ app.post("/bfhl", (req, res) => {
         is_success: true,
         user_id: "yash_rai_07022005",
         email: "22BCS16285@cuchd.in",
-        roll_number:"22BCS16285",
+        roll_number: "22BCS16285",
         numbers,
         alphabets,
         highest_alphabet: highestAlphabet
