de3d07fe625067c7dec9404a1078e39131fd6386
Yash <starrai843@gmail.com>
2025-02-21 16:41:57 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); 
+
+app.post("/bfhl", (req, res) => {
+    const { data } = req.body;
+    
+    const numbers = data.filter((item) => !isNaN(item));
+    const alphabets = data.filter((item) => isNaN(item));
+    const highestAlphabet = alphabets.length ? alphabets.sort().pop().toUpperCase() : null;
+
+    res.json({
+        is_success: true,
+        user_id: "yash_rai_07022005",
+        email: "22BCS16285@cuchd.in",
+        roll_number:"22BCS16285",
+        numbers,
+        alphabets,
+        highest_alphabet: highestAlphabet
+    });
+});
+
+app.get("/bfhl", (req, res) => {
+    res.json({ operation_code: 1 });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
